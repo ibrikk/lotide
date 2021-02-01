975c16a78b4ed122f4b1c025911ac3773427955e
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-02-01 16:26:34 -0500

    creating head() function for lotide

--- head.js
+++ head.js
@@ -9,3 +9,10 @@ const assertEqual = function (actual, expected) {
 // TEST CODE
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
+
+const head = (arr) => {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
