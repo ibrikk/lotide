080a904c40cf2481a2c8b4aeb1d1b9d6493068bf
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-02-03 00:26:35 -0500

    created the file and wrote the flatten() function

---
+++ flatten.js
@@ -0,0 +1,38 @@
+/* const mainArr = [[1, 2], 3, [4, 5], 6];
+
+const flatten = function(arr) {
+  let arr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      arr2 = arr2.concat(flatten(arr[i]))
+    } else {
+      arr2.push(arr[i])
+    }
+  }
+  return arr2;
+}
+
+console.log(flatten(mainArr));
+*/
+
+const flatten = function(array) {
+
+  let newArr = [];
+  
+  for (let i = 0; i < array.length; i ++) {
+    if (Array.isArray(array[i])) { 
+      for (let j = 0; j < array[i].length; j ++) {
+        newArr.push(array[i][j]);
+      }
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+
+  return newArr;
+
+}
+
+const mainArr = [[1, 2], 3, [4, 5], 6];
+console.log(flatten(mainArr));
