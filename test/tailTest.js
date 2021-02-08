9c2a7ba7fbadd865f57673f7e02b168d56079f7f
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-02-08 17:31:11 -0500

    created a testing file for middle and perfected it

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const res = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(res.length, 2); // ensure we get back two elements
+// assertEqual(res[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(res[1], "Labs");
+
+const assert = require("chai").assert;
 const tail = require('../tail');
 
-const res = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(res.length, 2); // ensure we get back two elements
-assertEqual(res[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(res[1], "Labs");
+describe("#tail", () => {
+  it("returns lighthouse and labs", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), [ 'Lighthouse', 'Labs' ]);
+  });
+});
